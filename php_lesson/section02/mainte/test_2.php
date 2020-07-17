75255d550763afea4e9ec67063d44a760e06a5be
sugimoto <natsumi-sugimoto@upload-gp.co.jp>
2020-07-17 11:37:51 +0900

    200716

---
+++ php_lesson/section02/mainte/test_2.php
@@ -0,0 +1,28 @@
+<?php
+
+$contactFile = '.contact.dat';
+
+// ファイル丸ごと読み込み
+$fileContents = file_get_contents($contactFile);
+
+// echo $fileContents;
+
+// ファイルに書き込み(上書き)
+// file_put_contents($contactFile, 'テストです');
+
+// 改行 -> \r\n
+$addText = 'テストです' . "\r\n";
+
+// ファイルに書き込み(追記)
+file_put_contents($contactFile, $addText, FILE_APPEND);
+
+
+// 配列 file, 区切る explode, foreach
+
+$allData = file($contactFile);
+
+foreach($allData as $lineData){
+    $
+}
+
+?>
